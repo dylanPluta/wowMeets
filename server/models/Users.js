62700d99d14a8a6bfe2a2dab860cb88d2bbd0661
Dylan Pluta <44102242+dylanPluta@users.noreply.github.com>
2023-07-13 13:30:16 -0400

    Delete server directory

--- server/models/Users.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose")
-
-const UserSchema = new mongoose.Schema({
-    userName: String,
-    id: Number
-});
-
-const UserModel = mongoose.model("users", UserSchema);
-module.exports = UserModel;
